2a378bb767b025f0548db7a892ba1bf92fad05ec
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2023-04-20 12:50:38 +0300

    Update sphere1.pas

--- 5 .Sub Programs/Functions/sphere1.pas
+++ 5 .Sub Programs/Functions/sphere1.pas
@@ -1,22 +1,21 @@
-program sphere1;
+program sphere1; 
 uses crt;
-const
-pi=3.412;
-var 
-r , volume :Real;
-function vol(x:Real):Real;
- var result:Real;
- begin 
- WriteLn('enter the radius');
- readln(r);
- result:=(4/3)*r**3;
- vol:=result;
- end;
-begin
-WriteLn('enter radius');
-readln(r);
-WriteLn('volume of a sphere is ', vol(r));
-
-
-readln;
+const 
+pi=3.412; 
+var
+r,volume:Real; 
+function vol(x:Real):Real; 
+  var result:Real; 
+  begin
+  WriteLn('enter the radius'); 
+  readln(r); 
+  result:=(4/3)*r*r*r; 
+  vol:=result; 
+  end; 
+begin 
+ 
+ WriteLn('volume of a sphere is ',vol(r),'cm'); 
+  
+  
+ readln; 
 end.
